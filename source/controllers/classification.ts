c078e7dfa6ed6aab2248b5b0c3fc2d8d69617462
viniFurukawa <vinicius.carvalho@opus-software.com.br>
2022-10-06 11:53:44 -0300

    Fixing bug on classification

--- source/controllers/classification.ts
+++ source/controllers/classification.ts
@@ -5,7 +5,7 @@ const retrieve = async (req: Request, res: Response, next: NextFunction) => {
     const classification = await getClassification();
     if (classification.size === 0) return res.status(404).json()
 
-    return res.status(200).json(classification.docs[0])
+    return res.status(200).json(classification.docs[0].data())
 }
 
 export default {
